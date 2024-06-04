46dee342ca6e110e501aa89a92283ec1a113b6b2
Dhananjaya Chamod <171498451+Dhananjaya001@users.noreply.github.com>
2024-06-04 21:24:58 +0530

    Update 121Best Time to Buy and Sell Stock.py

--- 121Best Time to Buy and Sell Stock.py
+++ 121Best Time to Buy and Sell Stock.py
@@ -1,22 +1,11 @@
 class Solution(object):
     def maxProfit(self, prices):
         mn=prices[0]
-        c=0
-        for i in range (1,len(prices)):
+        mxp=0
+        for i in range(1,len(prices)):
             if prices[i]<mn:
                 mn=prices[i]
-                c=i
-        sell =prices[c+1:]
-        if len(sell)==0:
-            return 0
-        else:
-            maxp=0
-            for s in sell:
-                p=s-mn
-                if p>maxp:
-                    maxp=p
-            return maxp
+            if mxp<prices[i]-mn:
+                mxp=prices[i]-mn
 
-
-        
-        
+        return mxp
